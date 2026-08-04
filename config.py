c798ef0fbd54f9abada87d148d7c5ac7233455da
tseqx-wm24-pixel <tseqx-wm24@student.tarc.edu.my>
2026-08-04 15:01:33 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
-customregion = "us-east-1"
-
