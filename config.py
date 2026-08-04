1a3c1974afa0f06a252a86674e7fda864df86c37
tseqx-wm24-pixel <tseqx-wm24@student.tarc.edu.my>
2026-08-04 15:01:54 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cg7bv3gxwsxq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tseqx-employee"
+customregion = "us-east-1"
+
